43e30b3d02f6ec577aa0742f7342983f61fd2f41
jobayer58 <jobayerafnan770@gmail.com>
2025-10-05 22:53:46 +0600

    added banner without responsive

--- src/Components/Pages/Banner.jsx
+++ src/Components/Pages/Banner.jsx
@@ -1,14 +1,91 @@
 import React from 'react';
+import mern from '../../assets/mern.png'
+import marketing from '../../assets/marketing.png'
+import wordpress from '../../assets/wordpress.png'
+import system from '../../assets/system.png'
+// import imgIcon from '../../assets/Polygon 2.png'
+import fluter from '../../assets/fluter.png'
+import figma from '../../assets/figma.png'
+import { IoCall } from 'react-icons/io5';
+import arabic1 from '../../assets/image 9.png'
+import arabic2 from '../../assets/Layer_1.png'
+import frame from '../../assets/Frame 1618873276.png'
+
+
 
 const Banner = () => {
     return (
-        <div>
-            <div className='relative h-96' style={{
-        background: "linear-gradient(90deg, #D3EAFF 0%, #FFFFFF 50%, #D3EAFF 100%)"
-      }}>
+        <section>
+            <div className='relative h-[950px] py-30' style={{
+                background: "linear-gradient(90deg, #D3EAFF 0%, #FFFFFF 50%, #D3EAFF 100%)"
+            }}>
+                {/* 1st text */}
+                <div className='flex justify-center items-center gap-20 py-5'>
+                    {/* icon1 */}
+                    <div>
+                        <img src={mern} alt="" />
+                    </div>
+                    {/* text */}
+                    <div className=''>
+                        <h1 className='text-3xl yatraOne text-[#0062FF] '>Looking for Your One Stop Solution? </h1>
+                    </div>
+                    {/* icon2 */}
+                    <div>
+                        <img src={marketing} alt="" />
+                    </div>
+                </div>
+                {/* 2nd text */}
+                <div className='flex justify-center  gap-30 py-3'>
+                    {/* icon 1 */}
+                    <div>
+                        <img src={wordpress} alt="" />
+                    </div>
+                    {/* text */}
+                    <div>
+
+                        <h1 className='text-6xl text-center font-bold leading-20'>Complete Digital Solutions  <br />for Your Business</h1>
+
+                    </div>
+                    {/* icon2 */}
+                    <div>
+                        <img src={system} alt="" />
+                    </div>
+                </div>
+                {/* button */}
+                <div className='flex justify-center items-center gap-2 py-8'>
+                    <button className='bg-blue-900 text-white py-1 px-4 rounded-xl'>Web Application</button>
+                    <button className='bannerBtn'>Mobile Application</button>
+                    <button className='bannerBtn'>API Integration </button>
+                    <button className='bannerBtn'>Digital Marketing</button>
+                    <button className='bannerBtn'>UI/UX</button>
+                    <button className='bannerBtn'>Domain Hosting</button>
+                    <button className='bannerBtn'>Tech Consultency</button>
+                </div>
+                {/* button2 */}
+                <div className='flex justify-center items-center py-6 gap-40'>
+                    <div>
+                        <img src={fluter} alt="" />
+                    </div>
+                    <div className='flex gap-3 bg-blue-50 p-2 rounded-md'>
+                        <button className='bg-black text-white py-2 px-4 rounded-lg flex justify-center items-center gap-2'><IoCall className='bg-blue-500 text-xl p-1 h-6 w-6 rounded-md' /> Book an intro Call</button>
+                        <button className='bg-white rounded-lg border py-1 px-4 font-semibold'>Explore Our Works</button>
+                    </div>
+                    <div>
+                        <img src={figma} alt="" />
+                    </div>
+                </div>
+                {/* img */}
+                <div className='flex justify-center items-center gap-6'>
+                    <img src={arabic1} alt="" />
+                    <img src={arabic2} alt="" />
+                </div>
+                {/* frame */}
+                <div>
+                    <img src={frame} className='ml-20' alt="" />
+                </div>
 
             </div>
-        </div>
+        </section>
     );
 };
 
--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/web logo1 1 1.svg'
 // import icon from '../../assets/imgi_88_al-icon 1.png'
 import { Link } from 'react-router';
@@ -7,39 +7,52 @@ import { MdKeyboardArrowDown } from 'react-icons/md';
 import { IoApps } from 'react-icons/io5';
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
     return (
-        <div>
-            <nav className='flex justify-between items-center py-8 px-18 fixed top-0 left-0 w-full z-50 transition-all duration-300'>
-                {/* logo */}
+
+        <nav className={`flex justify-between items-center py-5 px-18 fixed top-0 left-0 w-full z-50 transition-all duration-300 ${isScrolled ? " bg-white" : "bg-transparent"
+            }`}>
+            {/* logo */}
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            {/* navigation */}
+            <div>
+                <ul className='gap-3 flex font-semibold'>
+                    <Link>Home </Link>/
+                    <Link className='navbarClass'>Services <MdKeyboardArrowDown /></Link>/
+                    <Link className='navbarClass'>Products <MdKeyboardArrowDown /></Link>/
+                    <Link className='navbarClass'>Digital Marketing <MdKeyboardArrowDown /></Link>/
+                    <Link className='navbarClass'>Web Hosting <MdKeyboardArrowDown /></Link>/
+                    <Link className='navbarClass'>About <MdKeyboardArrowDown /></Link>/
+                </ul>
+            </div>
+            {/* signup */}
+            <div className='flex gap-5 justify-center items-center'>
                 <div>
-                    <img src={logo} alt="" />
+                    <button class="btn  relative inline-flex  items-center justify-center rounded-xl overflow-hidden font-medium transition-all bg-black gap-2 group py-2.5 px-5">
+                        <span class="w-56 h-48 rounded bg-[#0062FF] absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span><PiSignIn className='text-white text-2xl relative transition-colors duration-300 ease-in-out ' />
+                        <span class="relative  w-full text-left text-white transition-colors duration-300 ease-in-out  font-semibold">My Account</span>
+                    </button>
                 </div>
-                {/* navigation */}
                 <div>
-                    <ul className='gap-3 flex font-semibold'>
-                        <Link>Home </Link>/            
-                        <Link className='navbarClass'>Services <MdKeyboardArrowDown /></Link>/            
-                        <Link className='navbarClass'>Products <MdKeyboardArrowDown /></Link>/
-                        <Link className='navbarClass'>Digital Marketing <MdKeyboardArrowDown /></Link>/
-                        <Link className='navbarClass'>Web Hosting <MdKeyboardArrowDown /></Link>/
-                        <Link className='navbarClass'>About <MdKeyboardArrowDown /></Link>/
-                    </ul>
+                    {/* <img src={icon} alt="" /> */}
+                    <IoApps className='text-3xl text-gray-400' />
                 </div>
-                {/* signup */}
-                <div className='flex gap-5 justify-center items-center'>
-                    <div>
-                        <button class="btn  relative inline-flex  items-center justify-center rounded-xl overflow-hidden font-medium transition-all bg-black gap-2 group py-2.5 px-5">
-                            <span class="w-56 h-48 rounded bg-[#0062FF] absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span><PiSignIn className='text-white text-2xl relative transition-colors duration-300 ease-in-out ' />
-                            <span class="relative  w-full text-left text-white transition-colors duration-300 ease-in-out  font-semibold">My Account</span>
-                        </button>
-                    </div>
-                    <div>
-                        {/* <img src={icon} alt="" /> */}
-                        <IoApps className='text-3xl text-gray-400' />
-                    </div>
-                </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
+
     );
 };
 
